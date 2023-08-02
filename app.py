f02744b3754ec420fc2e796c00be3689f4371fc5
Juan Martin Burgo Mangiantini <85910666+jmartinburgo@users.noreply.github.com>
2023-08-02 09:45:56 +0000

    Added API route

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
+from utils import make_prediction
 
 app = Flask(__name__)
-cv = pickle.load(open("models/cv.pkl",'rb'))
-clf = pickle.load(open("models/clf.pkl",'rb'))
+
 
 @app.route("/")
 def home():
@@ -12,19 +11,14 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     email = request.form.get('content')
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    prediction = 1 if prediction == 1 else -1
+    prediction = make_prediction(email)
     return render_template("index.html", prediction=prediction, email=email)
 
 @app.route('/api/predict', methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)  # Get data posted as a json
     email = data['content']
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    # If the email is spam prediction should be 1
-    prediction = 1 if prediction == 1 else -1
+    prediction = make_prediction(email)
     return jsonify({'prediction': prediction, 'email': email})  # Return prediction
 
 if __name__ == "__main__":
